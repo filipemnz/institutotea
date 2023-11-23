6354a978cfc43c2c7afe94eab9f67e2c5cf96b55
Filipe <143033247+filipemnz@users.noreply.github.com>
2023-11-23 04:17:25 -0300

    Update enviarform.js

--- enviarform.js
+++ enviarform.js
@@ -4,5 +4,5 @@ function enviar() {
     var emailUser = document.getElementById('emailUser');
     var questionUser = document.getElementById('questionUser');
     var messageUser = document.getElementById('messageUser');
-    window.open('https://api.whatsapp.com/send/?phone=5579981461514&text=*Nome:*%20' + nameUser.value + '%0ATelefone:%20' + telUser.value + '%0AE-mail:%20' + emailUser.value + '%0AAssunto:%20' + questionUser.value + '%0AMensagem:%20' + messageUser.value + '&type=phone_number&app_absent=0');
+    window.open('https://api.whatsapp.com/send/?phone=5579981461514&text=*Nome:*%20' + nameUser.value + '%0A*Telefone:*%20' + telUser.value + '%0A*E-mail:*%20' + emailUser.value + '%0A*Assunto:*%20' + questionUser.value + '%0A*Mensagem:*%20' + messageUser.value + '&type=phone_number&app_absent=0');
 }
