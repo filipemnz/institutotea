25556353e5ead8a385510d79c7265ac0a7c4d815
Filipe <143033247+filipemnz@users.noreply.github.com>
2023-11-24 23:37:01 -0300

    Update 5.9
    Mudança de ordem no formulário...

--- enviarformC.js
+++ enviarformC.js
@@ -4,5 +4,5 @@ function enviar() {
     var telUser = document.getElementById('telUser');
     var idadeUser = document.getElementById('idadeUser');
     var messageUser = document.getElementById('messageUser');
-    window.open('https://api.whatsapp.com/send/?phone=5579981461514&text=*Formulário%20de%20Pré-Cadastro:*%0A%0A*Nome:*%20' + nameUser.value + '%0A*Nome do Responsável:*%20' + nameUserP.value + '%0A*Telefone:*%20' + telUser.value + '%0A*Idade:*%20' + idadeUser.value + '%0A*Mensagem:*%20' + messageUser.value + '&type=phone_number&app_absent=0');
+    window.open('https://api.whatsapp.com/send/?phone=5579981461514&text=*Formulário%20de%20Pré-Cadastro:*%0A%0A*Nome:*%20' + nameUser.value + '%0A*Idade:*%20' + idadeUser.value + '%0A*Nome do Responsável:*%20' + nameUserP.value + '%0A*Número para Contato:*%20' + telUser.value + '%0A*Mensagem:*%20' + messageUser.value + '&type=phone_number&app_absent=0');
 }
